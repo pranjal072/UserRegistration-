e6437c4efd3dedbfe5f750dd9c7862affd7c123a
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-25 17:29:08 +0530

    RegEx used to validate Mobile Number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,13 +4,19 @@ echo "USER REGISTRATION:"
 
 shopt -s extglob
 
+#READ ENTRIES
 read -p "Enter First Name (Start with capital and min 3 characters) : " Fname
 read -p "Enter Last Name (Start with capital and min 3 characters) : " Lname
 read -p "Enter EMAIL ( Eg: xyz.abc@bl.co.in ): " Email
+read -p "Enter Mobile Number with Country code  ( Eg: 91 9087654321 ): " Mnum
+
+#PATTERNS
 FNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
 LNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
 Epattern="^[0-9A-Za-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]{1,}([.][a-zA-Z]{2,3}){1,2}$"
+Mpattern="^[0-9]{2} [0-9]{10}$"
 
+#FIRST NAME
 if [[ $Fname =~ $FNpattern ]]
 then
 	echo "First Name is Valid"
@@ -18,6 +24,7 @@ else
 	echo "First Name is Invalid"
 fi
 
+#LAST NAME
 if [[ $Lname =~ $LNpattern ]]
 then
   	echo "Last Name is Valid"
@@ -25,9 +32,20 @@ else
   	echo "Last Name is Invalid"
 fi
 
+#EMAIL
 if [[ $Email =~ $Epattern ]]
 then
    echo "Email is Valid"
 else
    echo "Email is Invalid"
 fi
+
+#MOBILE NUMBER
+if [[ $Mnum =~ $Mpattern ]]
+then
+   echo "Mobile Number is Valid"
+else
+   echo "Moblie Number is Invalid"
+fi
+
+
