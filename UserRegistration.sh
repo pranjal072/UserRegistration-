f5e23bf372c99d649bffef5269fe27092aac7370
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-25 17:44:20 +0530

    Regex used to validate Password for 8 characters or more

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -9,12 +9,14 @@ read -p "Enter First Name (Start with capital and min 3 characters) : " Fname
 read -p "Enter Last Name (Start with capital and min 3 characters) : " Lname
 read -p "Enter EMAIL ( Eg: xyz.abc@bl.co.in ): " Email
 read -p "Enter Mobile Number with Country code  ( Eg: 91 9087654321 ): " Mnum
+read -p "Enter Password ( Min 8 Characters ): " pass
 
 #PATTERNS
 FNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
 LNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
 Epattern="^[0-9A-Za-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]{1,}([.][a-zA-Z]{2,3}){1,2}$"
 Mpattern="^[0-9]{2} [0-9]{10}$"
+Ppattern="^.{8,}"
 
 #FIRST NAME
 if [[ $Fname =~ $FNpattern ]]
@@ -48,4 +50,10 @@ else
    echo "Moblie Number is Invalid"
 fi
 
-
+#PASSWORD
+if [[ $pass =~ $Ppattern ]]
+then
+   echo "Password is Valid"
+else
+   echo "Password is Invalid"
+fi
