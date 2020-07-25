1e2b6c468d505eeb0fb171cff2f5be62334f6080
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-25 15:57:34 +0530

    Regex used to Validate First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x 
 
 echo "USER REGISTRATION:"
+
+shopt -s extglob
+
+read -p "Enter First Name : " Fname
+FNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
+if [[ $Fname =~ $FNpattern ]]
+then
+	echo "First Name is Valid"
+else
+	echo "First Name is Invalid"
+fi
