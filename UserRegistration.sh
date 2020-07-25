a1d5eed6781ac73453374e25c5b8a17968bb6cb6
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-25 17:01:08 +0530

    RegEx used to validate Email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,8 +6,10 @@ shopt -s extglob
 
 read -p "Enter First Name (Start with capital and min 3 characters) : " Fname
 read -p "Enter Last Name (Start with capital and min 3 characters) : " Lname
+read -p "Enter EMAIL ( Eg: xyz.abc@bl.co.in ): " Email
 FNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
 LNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
+Epattern="^[0-9A-Za-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]{1,}([.][a-zA-Z]{2,3}){1,2}$"
 
 if [[ $Fname =~ $FNpattern ]]
 then
@@ -23,3 +25,9 @@ else
   	echo "Last Name is Invalid"
 fi
 
+if [[ $Email =~ $Epattern ]]
+then
+   echo "Email is Valid"
+else
+   echo "Email is Invalid"
+fi
