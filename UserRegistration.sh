15a80bfd4efb7041a32b6b9586c46d9ffcd47e04
pranjal072 <pranjaltyagi97@gmail.com>
2020-07-25 16:18:04 +0530

    RegEx used to validte Last Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,14 +1,25 @@
-#!/bin/bash -x 
+#!/bin/bash
 
 echo "USER REGISTRATION:"
 
 shopt -s extglob
 
-read -p "Enter First Name : " Fname
+read -p "Enter First Name (Start with capital and min 3 characters) : " Fname
+read -p "Enter Last Name (Start with capital and min 3 characters) : " Lname
 FNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
+LNpattern="^[A-Z]{1}[A-Za-z]{2,}$"
+
 if [[ $Fname =~ $FNpattern ]]
 then
 	echo "First Name is Valid"
 else
 	echo "First Name is Invalid"
 fi
+
+if [[ $Lname =~ $LNpattern ]]
+then
+  	echo "Last Name is Valid"
+else
+  	echo "Last Name is Invalid"
+fi
+
